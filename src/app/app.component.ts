236c65a5caa3be0588bd6054cdb6479974cc44e0
Walter <Tano.walter@hotmail.com>
2022-11-30 20:07:10 -0300

    app-muestra-mejorada

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+  IP = 'http://192.168.255.1:3000';
+  imgUrl = '';
+  isFetching = false;
   title = 'app-muestra';
+
+  doFetch() {
+    this.isFetching = true;
+    fetch(this.IP)
+    .then(() => {
+        this.isFetching = false;
+        this.imgUrl = 'https://pbs.twimg.com/media/EY6EnxgWoAItjDp.jpg';
+    })
+    .catch(() => {
+      this.isFetching = false;
+      this.imgUrl = 'https://i.pinimg.com/474x/ee/12/a9/ee12a906d097550141060360ccc54fd2.jpg';
+    });
+  }
 }
